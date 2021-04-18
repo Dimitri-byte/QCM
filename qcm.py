1d627ea18de1cd2c6cdfb4f0f5f0d156edefe8c0
Dimitri-byte <Dm%7$7TjwSS^S>
2021-04-18 21:35:04 +0200

    21h34

--- qcm.py
+++ qcm.py
@@ -1,1 +1,10 @@
-init
+# -*- coding: utf-8 -*-
+import yaml
+
+# with open(r'G:\Mon Drive\QCM\qcm.yml') as file:
+#     question = yaml.load(file, Loader=yaml.FullLoader)
+#     print(question)
+
+with open(r'G:\Mon Drive\QCM\qcm.yml') as file:
+    question = yaml.full_load(file)
+    print(question)
